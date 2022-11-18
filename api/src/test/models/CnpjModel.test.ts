53a691bc50f1f703b43266b3305fbe8f9763460a
Rogério M. Queiroz <rogerio.mq@gmail.com>
2022-11-18 20:58:36 -0300

    ref(api): refactor Cnpj test

--- api/src/test/models/CnpjModel.test.ts
+++ api/src/test/models/CnpjModel.test.ts
@@ -8,42 +8,49 @@ import {
 } from '../../../dev_deps.ts';
 import { Cnpj } from '../../models/CnpjModel.ts';
 
-describe('Model -> CNPJ', () => {
-  describe('when a default instance: ', () => {
-    const cnpj = Cnpj.instance();
-    it('id is UNDEFINED and value is an STRING EMPTY', () => {
-      assertEquals(cnpj.id, undefined);
-      assertEquals(cnpj.value, '');
-      assertExists(cnpj.validate, 'Not found validate method.');
-      assertInstanceOf(cnpj, Cnpj);
-    });
+const makeSut = () => {
+  return {
+    sut: Cnpj.instance(),
+  };
+};
 
-    it('validate function return error[CNPJ not is valid]', () => {
-      assertEquals(cnpj.validate(), 'Invalid CNPJ');
-    });
+describe('Model -> CNPJ', () => {
+  const { sut } = makeSut();
+  it('Should id UNDEFINED and value is empty when call default instance', () => {
+    assertEquals(sut.id, undefined);
+    assertEquals(sut.value, '');
+    assertExists(sut.validate, 'Not found validate method.');
+    assertInstanceOf(sut, Cnpj);
   });
 
-  describe('when a new invalid CNPJ instance:', () => {
-    const cnpj = new Cnpj({ value: '68.326.854/7352-04' });
-    it('failure validate', () => {
-      const validateResult = cnpj.validate();
-      assertEquals(typeof validateResult, 'string');
-      assertEquals(validateResult, 'Invalid CNPJ');
-    });
+  it('Should return error[CNPJ not is valid] when call validate method with default instance', () => {
+    assertEquals(sut.validate(), 'Invalid CNPJ');
+  });
+
+  it('Should failure validate, return a error text with invalid CNPJ', () => {
+    const { sut } = makeSut();
+    sut.value = '68.326.854/7352-04';
+    const validateResult = sut.validate();
+    assertEquals(typeof validateResult, 'string');
+    assertEquals(validateResult, 'Invalid CNPJ');
   });
 
   describe('when a valid CNPJ instance:', () => {
-    const strCnpj = generateCnpj();
-    const onlyDigits = strCnpj.replace(/[^\d]/g, '');
-    const cnpj = new Cnpj({ value: onlyDigits });
-    it('validate return null', () => {
-      const validateResult = cnpj.validate();
+    const onlyDigits = generateCnpj().replace(/[^\d]/g, '');
+    const { sut } = makeSut();
+    sut.value = onlyDigits;
+
+    it('Should validate method return null', () => {
+      const validateResult = sut.validate();
       assertEquals(typeof validateResult, 'object');
       assertEquals(validateResult, null);
     });
+  });
 
-    it('cleaned return only numbers', () => {
-      assertEquals(cnpj.cleaned(), onlyDigits);
-    });
+  it('Should cleaned method return only numbers', () => {
+    const onlyDigits = generateCnpj().replace(/[^\d]/g, '');
+    const { sut } = makeSut();
+    sut.value = onlyDigits;
+    assertEquals(sut.cleaned(), onlyDigits);
   });
 });
